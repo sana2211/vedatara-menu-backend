398512e03c612c43ddd05053f34f89c576ed845c
Sana Siddiqui <sanafaizsiddiqui@me.com>
2021-04-28 01:14:56 +0400

    msg

--- migrations/20210314185902_create_menus.js
+++ migrations/20210314185902_create_menus.js
@@ -5,8 +5,11 @@ exports.up = function(knex) {
     table.increments();
     table.integer("user_id").notNullable();
     table.string('title').notNullable();
+    table.string('type').notNullable();
     table.string('description').notNullable();
-    table.string('calories').notNullable();
+    table.integer('calories').notNullable();
+    table.integer('price').notNullable();
+
     table.timestamp('created_at').defaultTo(knex.fn.now());
     table.timestamp('updated_at').defaultTo(knex.fn.now());
     table.foreign("user_id").references("id").inTable("users").onUpdate("CASCADE").onDelete("CASCADE");
